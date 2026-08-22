86cffb27eeaa1b4952c7a23d4e2637bd5f9f2f73
Sumit2k3 <sumitsingh2k3@gmail.com>
2026-08-22 14:37:21 +0530

    Implement constructor injection with collections
    Implemented constructor injection for Employee with Address, List, Set, and Map dependencies using Spring XML configuration.

--- src/main/java/learning/spring/main/Main.java
+++ src/main/java/learning/spring/main/Main.java
@@ -10,8 +10,8 @@ public class Main {
 		
 		ApplicationContext ioc = new ClassPathXmlApplicationContext("application-context.xml");
 		
-		Employee bean1 = ioc.getBean("emp1", Employee.class);
-		System.out.println(bean1);
+//		Employee bean1 = ioc.getBean("emp1", Employee.class);
+//		System.out.println(bean1);
 		
 		Employee bean2 = ioc.getBean("emp2", Employee.class);
 		System.out.println(bean2);
--- src/main/java/learning/spring/model/Employee.java
+++ src/main/java/learning/spring/model/Employee.java
@@ -1,5 +1,9 @@
 package learning.spring.model;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Employee {
 	
 	private int id;
@@ -7,19 +11,53 @@ public class Employee {
 	
 	private Address address;
 	
+	private List<String> list;
+	
+	private Set<Integer> set;
+	
+	private Map<Integer, String> map;
+	
 	public Employee() {
 		System.out.println("Default Constructor Called Of Employee");
 	}
+	
+	public Employee(int id, String name, String gender, Address address, List<String> list, Set<Integer> set,
+			Map<Integer, String> map) {
 
-	public Employee(int id, String name, String gender, Address address) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.gender = gender;
 		this.address = address;
-		System.out.println("Employee.Employee(4)");
+		this.list = list;
+		this.set = set;
+		this.map = map;
 	}
 	
+	
+	public List<String> getList() {
+		return list;
+	}
+
+	public void setList(List<String> list) {
+		this.list = list;
+	}
+
+	public Set<Integer> getSet() {
+		return set;
+	}
+
+	public void setSet(Set<Integer> set) {
+		this.set = set;
+	}
+
+	public Map<Integer, String> getMap() {
+		return map;
+	}
+
+	public void setMap(Map<Integer, String> map) {
+		this.map = map;
+	}
+
 	public int getId() {
 		return id;
 	}
@@ -54,7 +92,10 @@ public class Employee {
 
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", address=" + address + "]";
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", address=" + address + ", list="
+				+ list + ", set=" + set + ", map=" + map + "]";
 	}
+
+	
 
 }
